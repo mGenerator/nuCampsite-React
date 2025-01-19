d68b94d2940649c5f00ce51569c5296ac6ac9a16
Calherbe Valcin <mgenerator01@hotmail.com>
2025-01-19 16:13:02 -0500

    CampsiteDetailPage

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -7,7 +7,7 @@ export const selectAllCampsites = ()=>{
 //   return CAMPSITES[Math.floor(Math.random()*CAMPSITES.length)];
 // };
 export const selectCampsiteById = (id)=>{
-  return CAMPSITES.find((campsite)=> campsite.id === id);
+  return CAMPSITES.find((campsite)=> campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = ()=>{
