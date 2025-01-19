23c0b1fd61ae132677aa498cb345e4434a657e88
Calherbe Valcin <mgenerator01@hotmail.com>
2025-01-19 10:44:40 -0500

    first commit, recreated project due to local issue

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,11 @@
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+export const selectAllCampsites = ()=>{
+  return CAMPSITES;
+};
+
+// export const selectRandomCampsite = ()=>{
+//   return CAMPSITES[Math.floor(Math.random()*CAMPSITES.length)];
+// };
+export const selectCampsiteById = (id)=>{
+  return CAMPSITES.find((campsite)=> campsite.id === id);
+};
