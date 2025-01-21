a832d331ae15bfd4f7ce39080bbde3ae8d9337db
Calherbe Valcin <mgenerator01@hotmail.com>
2025-01-21 15:39:50 -0500

    FeaturedPartner component and AboutPage

---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,20 @@
+import Partner from "./Partner";
+import { selectAllPartners } from "./partnersSlice";
+import { Col } from "reactstrap";
+
+const PartnersList = ()=>{
+  const partners = selectAllPartners();
+    return(
+      <Col className='mt-4'>
+        {partners.map((partner)=>{
+          return (
+            <div className="d-flex mb-5" key={partner.id}>
+              <Partner partner={partner} />
+            </div>
+          )
+        })}
+      </Col>
+    )
+
+};
+export default PartnersList;
