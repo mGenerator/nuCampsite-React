7e05efd42f86a3c98a3813675b567f212ae53ec6
Calherbe Valcin <mgenerator01@hotmail.com>
2025-01-31 15:44:20 -0500

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,21 +1,28 @@
+
+import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { addComment } from "./commentsSlice";
 
 const CommentForm = ({campsiteId})=>{
   const [modalOpen, setModalOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const handleSubmit = (values)=>{
     console.log(values.id);
     const comment = {
-      // id: values.id,
+      id: values.id,
       campsiteId: parseInt(campsiteId),
       rating: parseInt(values.rating),
       text: values.commentText,
-      author: values.author
+      author: values.author,
+      date: new Date(Date.now()).toISOString()
+
     };
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   }
   return(
--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -4,10 +4,30 @@ import { COMMENTS } from "../../app/shared/COMMENTS";
 const initialState = {
   commentsArray: COMMENTS
 }
+//the state in the following case is only the commentsSlice, not the entire state. just that slice
+//for that reason you write state.commentsArray ('state' is allready referencing comments)
 const commentsSlice = createSlice({
   name: 'comments',
-  initialState
+  initialState,
+  reducers:{
+    addComment: (state, action)=>{
+      console.log('add comment action.payload:', action.payload);
+      console.log('add comment state.commentsArray:', state.commentsArray);
+      const newComment = {
+        id: state.commentsArray.length + 1,
+        ...action.payload
+      }
+      //Redux toolkit implements the Immer Library. Allow for mutating operations on immutable objects
+      //this is a convenient sytax. Nothing is actually mutated
+      state.commentsArray.push(newComment);
+      //Redux toolkit and Immer will handle updating the state in an immutable way with this information
+      // Each Function defined in the reducers argument will also have a corresponding action creator function with the same function name
+
+    }
+  }
 });
+//here we are destructuring the addComment action creater function from the commentsSlices action property. This property was created due to createSlice
+export const {addComment} = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
 
 export const selectCommentsByCampsiteId = (campsiteId)=> (state)=>{
